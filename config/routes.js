7948512051ba09bb8105e7007b3e43600b644ea4
Agustina Sosa <agustinasf85@gmail.com>
2019-07-26 11:56:20 -0400

    Created register and login endpoints with tokens

--- config/routes.js
+++ config/routes.js
@@ -26,13 +26,32 @@ function register(req, res) {
   db.add(user)
     .then(saved => {
       const token = generateToken(saved);
-
+      res.status(201).json({ message: `Welcome ${saved.username}!`, token })
     })
-}
+    .catch( err => {
+      res.status(500).json(err);
+    })
+};
 
 function login(req, res) {
   // implement user login
-}
+  let { username , password } = req.body;
+
+  db.findBy({ username })
+    .first()
+    .then( user => {
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({ message: `Welcome back ${user.username}!, I have a token`, token })
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials ' })
+      }
+    })
+    .catch( err => {
+      res.status(500).json(err);
+    })
+};
 
 function getJokes(req, res) {
   const requestOptions = {
